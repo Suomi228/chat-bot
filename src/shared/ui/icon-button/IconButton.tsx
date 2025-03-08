4f1a6200b534023248230f89405dbcbf1238cf0a
Suomi228 <bixep345@gmail.com>
2025-03-08 20:06:59 +0300

    icon buttons

---
+++ src/shared/ui/icon-button/IconButton.tsx
@@ -0,0 +1,30 @@
+import "./index.css";
+import vector from "../../../assets/Vector.svg";
+import search from "../../../assets/search-simple.svg";
+import addChat from "../../../assets/add-chat.svg";
+
+type IconButtonProps = {
+  icon: string;
+  onClick?: () => void;
+  backgroundColor?: "blue" | "transparent";
+};
+
+export default function IconButton({
+  icon,
+  onClick,
+  backgroundColor,
+}: IconButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className={
+        backgroundColor === "blue"
+          ? "icon-button blue"
+          : "icon-button transparent"
+      }
+      type="button"
+    >
+      <img src={icon} alt="icon" className="icon-button__image" />
+    </button>
+  );
+}
--- src/widgets/sidebar/Sidebar.tsx
+++ src/widgets/sidebar/Sidebar.tsx
@@ -1,6 +1,10 @@
 import "./index.css";
 import logo from "../../assets/logo.svg";
 import UserInfo from "../../shared/ui/user-info/UserInfo";
+import IconButton from "../../shared/ui/icon-button/IconButton";
+import vector from "../../assets/Vector.svg";
+import search from "../../assets/search-simple.svg";
+import addChat from "../../assets/add-chat.svg";
 export default function Sidebar() {
   return (
     <div className="sidebar-tab">
@@ -8,7 +12,13 @@ export default function Sidebar() {
         <div className="sidebar__up-content">
           <img src={logo} alt="logo" />
         </div>
-      <UserInfo/>
+        <div className="sidebar__chat">
+          <div className="sidebar__icons">
+              <IconButton icon={addChat} backgroundColor="blue" />
+              <IconButton icon={search} backgroundColor="transparent" />
+          </div>
+        </div>
+        <UserInfo />
       </div>
     </div>
   );
