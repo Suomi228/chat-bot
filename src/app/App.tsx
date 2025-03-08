63c211fe02a9052e73eb52abd13563923dca218d
Suomi228 <bixep345@gmail.com>
2025-03-08 14:14:13 +0300

    fix widht and height

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import AuthPage from '../pages/auth-page/AuthPage'
+import DashboardPage from '../pages/dashboard/DashboardPage'
 function App() {
   
   return (
     <div className="container">
-      <AuthPage/>
+      {/* <AuthPage/> */}
+      <DashboardPage/>
     </div>
   )
 }
---
+++ src/pages/dashboard/DashboardPage.tsx
@@ -0,0 +1,14 @@
+import "./index.css";
+import Sidebar from "../../widgets/sidebar/Sidebar";
+export default function DashboardPage() {
+  return (
+    <div className="dashboard-page">
+      <aside className="sidebar">
+        <Sidebar />
+      </aside>
+      <main className="content">
+        Контент
+        </main>
+    </div>
+  );
+}
