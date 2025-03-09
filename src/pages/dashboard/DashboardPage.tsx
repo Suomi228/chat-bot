e59fbf216df2c70ce8cb274ddbbc1402a62ba1be
Suomi228 <bixep345@gmail.com>
2025-03-09 16:20:03 +0300

    responsive dashboard

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,13 +1,25 @@
 import "./index.css";
 import Sidebar from "../../widgets/sidebar/Sidebar";
 import Chat from "../../widgets/chat/Chat";
+import { useState } from "react";
+import OpenIcon from "../../assets/add-chat.svg";
+import IconButton from "../../shared/ui/icon-button/IconButton";
 export default function DashboardPage() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
     <div className="dashboard-page">
-      <aside className="sidebar">
+      <aside className={`sidebar ${isSidebarOpen ? "active" : ""}`}>
         <Sidebar />
       </aside>
-      <main className="content"><Chat/></main>
+      {isSidebarOpen && (
+        <div className="sidebar-overlay" onClick={() => setIsSidebarOpen(!isSidebarOpen)} />
+      )}
+      <main className="content">
+        <div className="burger-button">
+          <IconButton icon={OpenIcon} onClick={() => setIsSidebarOpen(!isSidebarOpen)} backgroundColor="blue"></IconButton>
+        </div>
+        <Chat />
+      </main>
     </div>
   );
 }
